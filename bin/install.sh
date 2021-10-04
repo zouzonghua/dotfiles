aff7129d3d3efea9944f73590011c9acad5d5e53
zouzonghua <zouzonghua.cn@gmail.com>
2021-10-04 20:21:18 +0800

    feat: add yabai, skhd

--- bin/install.sh
+++ bin/install.sh
@@ -1,3 +1,12 @@
+action "Install yabai and skhd"
+brew install koekeishiya/formulae/yabai
+brew install koekeishiya/formulae/skhd
+sudo yabai --install-sa
+ln "${HOME}/.dotfiles/yabai/.yabairc" "${HOME}/.yabairc"
+ln "${HOME}/.dotfiles/yabai/.skhdrc" "${HOME}/.skhdrc"
+brew services start skhd
+brew services start yabai
+
 ln -s $HOME/.dotfiles/tmux/.tmux.conf $HOME/.tmux.conf
 ln -s $HOME/.dotfiles/git/.gitconfig $HOME/.gitconfig
 ln -s $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc
