b0bbe4a8b5695488307b5fab938969a343625564
zouzonghua <zouzonghua.cn@gmail.com>
2021-10-06 08:47:39 +0800

    docs: update font install

--- bin/install.sh
+++ bin/install.sh
@@ -17,6 +17,8 @@ ln $HOME/.dotfiles/rime/squirrel.custom.yaml  $HOME/Library/Rime/squirrel.custom
 ln $HOME/.dotfiles/rime/luna_pinyin_simp.custom.yaml  $HOME/Library/Rime/luna_pinyin_simp.custom.yaml
 ln $HOME/.dotfiles/rime/luna_pinyin.extended.dict.yaml  $HOME/Library/Rime/luna_pinyin.extended.dict.yaml
 ln $HOME/.dotfiles/rime/luna_pinyin.sougou.dict.yaml  $HOME/Library/Rime/luna_pinyin.sougou.dict.yaml
+cp $HOME/.dotfiles/rime/fonts/HanaMinA.ttf $HOME/Library/Fonts
+cp $HOME/.dotfiles/rime/fonts/HanaMinB.ttf $HOME/Library/Fonts
 
 cd ~/Library/Fonts && curl -fLo "Meslo LG M Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Meslo/M/Regular/complete/Meslo%20LG%20M%20Regular%20Nerd%20Font%20Complete.ttf
 cd ~/Library/Fonts && curl -fLo "Meslo LG M Regular Nerd Font Complete Mono.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Meslo/M/Regular/complete/Meslo%20LG%20M%20Regular%20Nerd%20Font%20Complete%20Mono.ttf
