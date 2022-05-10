e6944154f5466c3344f1f254a106e5e622030bbf
zouzonghua <zouzonghua.cn@gmail.com>
2022-05-10 17:28:35 +0800

    feat: add pomodoor dialog alert

---
+++ .hammerspoon/modules/application_watcher.lua
@@ -0,0 +1,35 @@
+-- Application Watcher
+local module = {}
+module.logger = hs.logger.new("applicationWatcher.lua")
+
+-- deactivatedApps array
+_G.var = {}
+_G.var.deactivatedApps = {}
+
+-- NOTE: Use local function for any internal funciton not exported (not included as a part of the return)
+local function applicationWatcher(appName, eventType, appObject)
+	if eventType == hs.application.watcher.deactivated then
+		table.insert(_G.var.deactivatedApps, appName)
+		if #_G.var.deactivatedApps >= 5 then
+			_G.var.deactivatedApps = {}
+		end
+	end
+end
+
+module.watcher = hs.application.watcher.new(applicationWatcher)
+
+module.start = function()
+	module.logger.i("Starting Application Watcher")
+	module.watcher:start()
+	return module
+end
+
+module.stop = function()
+	module.logger.i("Stopping Application Watcher")
+	module.watcher:stop()
+	return module
+end
+
+module.start()
+
+return module
--- .hammerspoon/modules/launcher.lua
+++ .hammerspoon/modules/launcher.lua
@@ -1,15 +1,15 @@
-local hotkey = require("hs.hotkey")
 local grid = require("hs.grid")
-local window = require("hs.window")
-local application = require("hs.application")
-local appfinder = require("hs.appfinder")
+local hotkey = require("hs.hotkey")
+-- local window = require("hs.window")
 local fnutils = require("hs.fnutils")
+-- local appfinder = require("hs.appfinder")
+local application = require("hs.application")
 
 local modifiers = { "ctrl", "alt" }
 
 grid.setMargins({ 0, 0 })
 
-applist = {
+local applist = {
 	{ hotkey = "A", appname = "Alacritty" },
 	{ hotkey = "G", appname = "Google Chrome" },
 	{ hotkey = "T", appname = "Telegram Desktop" },
@@ -18,7 +18,6 @@ applist = {
 	{ hotkey = "N", appname = "NeteaseMusic" },
 	{ hotkey = "Z", appname = "zoom.us" },
 	{ hotkey = "S", appname = "Spotify" },
-
 	{ hotkey = "L", appname = "Launchpad" },
 	{ hotkey = "F", appname = "Finder" },
 	{ hotkey = "B", appname = "Books" },
@@ -33,22 +32,23 @@ fnutils.each(applist, function(item)
 end)
 
 -- Toggle an application between being the frontmost app, and being hidden
-function toggle_application(_app)
-	local app = appfinder.appFromName(_app)
-	if not app then
-		application.launchOrFocus(_app)
-		return
-	end
-	local mainwin = app:mainWindow()
-	if mainwin then
-		if mainwin == window.focusedWindow() then
-			mainwin:application():hide()
-		else
-			mainwin:application():activate(true)
-			mainwin:application():unhide()
-			mainwin:focus()
-		end
-	else
-		application.launchOrFocus(_app)
-	end
-end
+-- local function toggle_application(_app)
+-- 	local app = appfinder.appFromName(_app)
+-- 	if not app then
+-- 		application.launchOrFocus(_app)
+-- 		return
+-- 	end
+-- 	local mainwin = app:mainWindow()
+-- 	if mainwin then
+-- 		if mainwin == window.focusedWindow() then
+-- 			mainwin:application():hide()
+-- 		else
+-- 			mainwin:application():activate(true)
+-- 			mainwin:application():unhide()
+-- 			mainwin:focus()
+-- 		end
+-- 	else
+-- 		application.launchOrFocus(_app)
+-- 	end
+-- end
+
