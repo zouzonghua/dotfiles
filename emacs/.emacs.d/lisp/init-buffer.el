20a8feb2cfe45aa9338c31e8aa248eea21d5590d
zonghua <zouzonghua.cn@gmail.com>
2024-06-25 14:51:14 +0800

    refactor: update

--- emacs/.emacs.d/lisp/init-buffer.el
+++
@@ -1,35 +0,0 @@
-;;; init-buffer.el --- Config for minibuffer completion       -*- lexical-binding: t; -*-
-;;; Commentary:
-;;; Code:
-
-
-(use-package vertico)
-(vertico-mode t)
-
-
-(use-package orderless)
-(setq completion-styles '(orderless))
-
-
-(use-package marginalia)
-(marginalia-mode t)
-
-
-(use-package embark
-  :bind
-  ("M-h b" . embark-bindings)
-  :custom
-  (embark-help-key "?"))
-
-
-(use-package consult)
-(global-set-key [remap goto-line] 'consult-goto-line)
-(global-set-key [remap isearch-forward] 'consult-line)
-(global-set-key [remap switch-to-buffer] 'consult-buffer)
-(global-set-key [remap project-find-regexp] 'consult-ripgrep)
-(global-set-key [remap project-switch-to-buffer] 'consult-project-buffer)
-
-
-
-(provide 'init-buffer)
-;;; init-buffer.el ends here
--- emacs/.emacs.d/lisp/init-markdown.el
+++
@@ -1,9 +0,0 @@
-;;; init-markdown.el --- Markdown support -*- lexical-binding: t -*-
-;;; Commentary:
-;;; Code:
-
-(use-package markdown-mode
-  :mode ("\\.md\\.html\\'" . markdown-mode))
-
-(provide 'init-markdown)
-;;; init-markdown.el ends here
